f2611e7b1800ce7adad018a2758b0225d2dddaf8
Akinbobola <71355010+phemmyblaze@users.noreply.github.com>
2025-02-19 15:49:03 +0100

    build fail fixed

---
+++ app/client/job-postings/[name]/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage, Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Jobs - Manage Your Freelance Projects',
+    description:
+      'Efficiently manage your jobs and collaborations with the Projects dashboard on Workhive. Track progress, milestones, and deadlines effortlessly.',
+};
+const JobDetailsPage:NextPage = () => {
+    return ( <>Hello</> );
+}
+ 
+export default JobDetailsPage;
