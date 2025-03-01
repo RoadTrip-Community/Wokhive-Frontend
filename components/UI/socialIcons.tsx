cc8d91115ff5e52467b0f36722660ea5d2543de0
codabytez <chidiobinna0001@gmail.com>
2025-03-01 21:18:48 +0100

    feat: add new social icons for Facebook, Figma, and Google; remove old icons

---
+++ components/UI/socialIcons.tsx
@@ -0,0 +1,40 @@
+// Import images
+import facebookBrand from '@/public/assets/svgs/social-icons/facebookBrand.svg';
+import facebookColorWithBrand from '@/public/assets/svgs/social-icons/facebookColorWithBrand.svg';
+import facebookColor from '@/public/assets/svgs/social-icons/facebookColor.svg';
+import googleBrand from '@/public/assets/svgs/social-icons/googleBrand.svg';
+import googleColorWithBrand from '@/public/assets/svgs/social-icons/googleBrand.svg';
+import googleColor from '@/public/assets/svgs/social-icons/googleColor.svg';
+import figmaBrand from '@/public/assets/svgs/social-icons/figmaBrand.svg';
+import figmaColorWithBrand from '@/public/assets/svgs/social-icons/figmaBrand.svg';
+import figmaColor from '@/public/assets/svgs/social-icons/figmaColor.svg';
+import dribbleBrand from '@/public/assets/svgs/social-icons/dribbleBrand.svg';
+import dribbleColorWithBrand from '@/public/assets/svgs/social-icons/dribbleColorWithBrand.svg';
+import dribbleColor from '@/public/assets/svgs/social-icons/dribbleColor.svg';
+import twitterBrand from '@/public/assets/svgs/social-icons/twitterBrand.svg';
+import twitterColorWithBrand from '@/public/assets/svgs/social-icons/twitterColorWithBrand.svg';
+import twitterColor from '@/public/assets/svgs/social-icons/twitterColor.svg';
+import appleBrand from '@/public/assets/svgs/social-icons/appleBrand.svg';
+import appleColorWithBrand from '@/public/assets/svgs/social-icons/appleColorWithBrand.svg';
+import appleColor from '@/public/assets/svgs/social-icons/appleColor.svg';
+
+export {
+  facebookBrand,
+  facebookColorWithBrand,
+  facebookColor,
+  googleBrand,
+  googleColorWithBrand,
+  googleColor,
+  figmaBrand,
+  figmaColorWithBrand,
+  figmaColor,
+  dribbleBrand,
+  dribbleColorWithBrand,
+  dribbleColor,
+  twitterBrand,
+  twitterColorWithBrand,
+  twitterColor,
+  appleBrand,
+  appleColorWithBrand,
+  appleColor,
+};
